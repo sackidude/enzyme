642bc8a7a95de89df396765b45938f98851ca36b
sackidude <nils.nilsen9@outlook.com>
2024-04-17 22:12:29 +0200

    componentization

--- src/app/account.rs
+++ src/app/account.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use leptos::*;
 use leptos_router::use_query_map;
 use serde::{Deserialize, Serialize};
@@ -32,6 +34,20 @@ async fn get_account(name: String) -> Result<Account, ServerFnError> {
 }
 
 #[component]
+fn LoadingProfile() -> impl IntoView {
+    view! { <p>"Loading User"</p> }
+}
+
+#[component]
+fn ErrorProfile() -> impl IntoView {
+    view! {
+        <p>
+            "Something went wrong, probably: Couldn't find that account, please try again."
+        </p>
+    }
+}
+
+#[component]
 pub fn AccountPage() -> impl IntoView {
     let params = use_query_map();
     let account_name =
@@ -42,10 +58,13 @@ pub fn AccountPage() -> impl IntoView {
     view! {
         <section class="account-viewer">
             // handles the loading
-            <Suspense fallback=move || view! { <p>"Loading User"</p> }>
+            <Suspense fallback=LoadingProfile>
                 // handles the error from the resource
                 <ErrorBoundary fallback=|_| {
-                    view! { <p>"Something went wrong"</p> }
+                    view! {
+                        // FIXME: Simplify this, ie dont wrap in view macro and ignore _.
+                        <ErrorProfile/>
+                    }
                 }>
                     {move || {
                         account
@@ -54,8 +73,7 @@ pub fn AccountPage() -> impl IntoView {
                                 account
                                     .map(move |account| {
                                         view! {
-                                            // the resource has a result
-                                            // successful call from the server fn
+                                            // the resource has a result and successful call from the server fn
                                             <p>
                                                 "User result filled in server and client: "
                                                 {account.in_game_name}
